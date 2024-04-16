309ea5dc0d31f2be1337dab23a8ad35c2fbc7bff
awswa <janghan@amazon.com>
2024-04-16 16:01:57 +0800

    update TG

--- tf_scenario_code/1_pre-condition-ec2/vars.tf
+++ tf_scenario_code/1_pre-condition-ec2/vars.tf
@@ -16,12 +16,12 @@ variable ami_id {
   type = list(string)
   # default = "ami-0c1f7b7eb05c17ca5"
   default = ["ami-0c031a79ffb01a803"]
-  description = "보안 팀이 승인한 Amazon Linux 2023 AMI ARM64 지원 AMI"
+  description = "보안 팀이 승인한 AMI만 사용"
 }
 
 variable ami_name_filter {
   type = string
   default = "*al2023*-arm64"
   # default = "*al2023*-x86_64"
-  description = "Amazon Linux 2023 AMI ARM64 지원 AMI"
+  description = "보안 팀이 승인한 Amazon Linux 2023 AMI ARM64 지원 AMI"
 }
