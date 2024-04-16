780bee8baae840ae8971a3c004058f2f88ff5a6e
awswa <janghan@amazon.com>
2024-04-16 09:25:27 +0800

    tf update

--- tf_scenario_code/3_sentinel-s3/main.tf
+++ tf_scenario_code/3_sentinel-s3/main.tf
@@ -19,12 +19,3 @@ resource "aws_s3_bucket_acl" "s3-bucket-acl" {
   bucket = aws_s3_bucket.s3-bucket-sentinel.id
   acl    = var.acl_disabled ? "private" : "public-read"
 }
-
-#resource "aws_s3_bucket_public_access_block" "s3-bucket-public-access" {
-#  bucket = aws_s3_bucket.s3-bucket-sentinel.id
-
-#  block_public_acls       = false
-#  block_public_policy     = false
-#  ignore_public_acls      = false
-#  restrict_public_buckets = false
-#}
