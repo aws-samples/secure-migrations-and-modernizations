7cae9426cabadbb64d3608c7e29672cbaa50f534
awswa <janghan@amazon.com>
2024-04-16 18:36:14 +0800

    update comments

--- tf_scenario_code/1_pre-condition-ec2/main.tf
+++ tf_scenario_code/1_pre-condition-ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "al2023_arm" {
     name = "image-id"
     values = var.ami_id
     # ami-0c031a79ffb01a803는 사용자가 배포하려는 x86_64 이미지
-    # ami-0c1f7b7eb05c17ca5는 보안팀이 검증하고 승인한 arm64 이미지
+    # ami-0c1f7b7eb05c17ca5는 사내 보안팀이 검증하고 승인한 arm64 이미지
   }
 }
 
@@ -21,7 +21,7 @@ resource "aws_instance" "ec2" {
     # AMI 이미지는 ARM 아키텍처만 사용해야 함
     precondition {
       condition     = data.aws_ami.al2023_arm.architecture == "arm64"
-      error_message = "AMI 이미지는 반드시 보안팀이 검증한 ami-0c1f7b7eb05c17ca5 이어야 합니다"
+      error_message = "AMI 이미지는 반드시 사내 보안팀이 검증한 ami-0c1f7b7eb05c17ca5 이어야 합니다"
     }
   }
   tags = {
