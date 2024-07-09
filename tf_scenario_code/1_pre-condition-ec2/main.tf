b159079fa94963799e770b6aa504ff12f0e58fb4
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2024-07-09 23:49:36 +0900

    ec2_postcondition 추가된 리소스 블럭 추가

--- tf_scenario_code/1_pre-condition-ec2/main.tf
+++ tf_scenario_code/1_pre-condition-ec2/main.tf
@@ -9,6 +9,26 @@ data "aws_ami" "ubuntu_22" {
   }
 }
 
+resource "aws_instance" "ec2" {
+  ami                         = data.aws_ami.ubuntu_22.id
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+
+  lifecycle {
+    precondition {
+      condition     = contains(["t3.micro", "t3.large", "m6i.midium", "m6i.large"], var.instance_type)
+      error_message = "허용된 instance_type 은 t3.micro, t3.large, m6i.midium, m6i.large 입니다."
+    }
+    precondition {
+      condition     = data.aws_ami.ubuntu_22.architecture == "x86_64"
+      error_message = "AMI 이미지는 aws_instance_type이 x86_64이므로 x86_64 아키텍쳐여야 합니다."
+    }
+  }
+  tags = {
+    Name = "Ubuntu_22.04"
+  }
+}
+
 resource "aws_instance" "ec2_postcondition" {
   ami                         = data.aws_ami.ubuntu_22.id
   instance_type               = var.instance_type
